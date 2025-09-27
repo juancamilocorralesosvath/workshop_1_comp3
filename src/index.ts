9db6ef6caa78b46d72c08f574e0c6f8772a80ba4
juancamilocorralesosvath <juancamilocorra@gmail.com>
2025-09-27 11:13:50 -0500

    fix: folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import { db } from "../db/connectionDB";
+import { db } from "./db/connectionDB";
 import { userRouter } from "./routes/user.routes";
 import { authRouter } from "./routes/auth.routes";
 
