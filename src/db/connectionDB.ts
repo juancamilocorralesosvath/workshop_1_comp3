5186bb10c7da28eefa3250453d8508ae5fd1f1dc
juancamilocorralesosvath <juancamilocorra@gmail.com>
2025-09-27 16:55:12 -0500

    removed changes in db file

--- src/db/connectionDB.ts
+++ src/db/connectionDB.ts
@@ -4,13 +4,13 @@ import { config } from 'dotenv'
 config()
 
 
-//const CLOUD_MONGO_URI = 'mongodb+srv://admin_user:35N2qci42thwJ3VB@cluster0.hiuxs5q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const CLOUD_MONGO_URI = 'mongodb+srv://admin_user:35N2qci42thwJ3VB@cluster0.hiuxs5q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 
-//const LOCAL_MONGO_URI = 'mongodb://localhost:27018/workshop'
+const LOCAL_MONGO_URI = 'mongodb://localhost:27018/workshop'
 
 
-const connectionString = process.env.MONGO_URI || 'pailas' 
+const connectionString = process.env.MONGO_URI || CLOUD_MONGO_URI
 console.log("🚀 ~ connectionString:", connectionString)
 
 console.log('🔗 Connecting to MongoDB:', connectionString.includes('cluster0') ? 'Cloud Database (Atlas)' : 'Local Database (Docker)')
